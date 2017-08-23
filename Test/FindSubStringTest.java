30b82a84275a7b7217f071c6b16203c76828ae26
sschahil <schahil31@gmail.com>
2017-08-23 00:39:39 -0700

    Initial Test Commit
    -First Commit for FindSubStringTest.java
    
    -Has 10 test cases that have hardcoded string values
    -There is a Before that will initialize each test case with a string
    passed to the constructor
    
    -The methods used in the test cases are
    	createSubStr()
    	print()
    
    -Then an assertEquals is used to determine that the application is
    giving back the correct maximum length of the substring

---
+++ Test/FindSubStringTest.java
@@ -0,0 +1,94 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FindSubStringTest {
+	
+	FindSubString subStr1;
+	FindSubString subStr2;
+	FindSubString subStr3;
+	FindSubString subStr4;
+	FindSubString subStr5;
+	FindSubString subStr6;
+	FindSubString subStr7;
+	FindSubString subStr8;
+	FindSubString subStr9;
+	FindSubString subStr10;
+	
+	@Before 
+	public void setUp() {
+		subStr1 = new FindSubString("abcabcbb");
+		subStr2 = new FindSubString("bbbbbbb");
+		subStr3 = new FindSubString("pwwkew");
+		subStr4 = new FindSubString("lkjioaaalkbi");
+		subStr5 = new FindSubString("albi");
+		subStr6 = new FindSubString("JamesJamesLolloallkjoilnllll");
+		subStr7 = new FindSubString("jk");
+		subStr8 = new FindSubString("a");
+		subStr9 = new FindSubString("alkjioiaj");
+		subStr10 = new FindSubString("ioajskjoidjlkalk");	
+	}
+	
+	@Test
+	public void testLongestSubstringCalc() {
+		
+		subStr1.createSubStr();
+
+		subStr1.print();
+		assertEquals(3, subStr1.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr2.createSubStr();
+
+		subStr2.print();
+		assertEquals(1, subStr2.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr3.createSubStr();
+
+		subStr3.print();
+		assertEquals(3, subStr3.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr4.createSubStr();
+
+		subStr4.print();
+		assertEquals(6, subStr4.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr5.createSubStr();
+
+		subStr5.print();
+		assertEquals(4, subStr5.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr6.createSubStr();
+		
+		subStr6.print();
+		assertEquals(8, subStr6.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr7.createSubStr();
+
+		subStr7.print();
+		assertEquals(2, subStr7.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr8.createSubStr();
+
+		subStr8.print();
+		assertEquals(1, subStr8.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr9.createSubStr();
+
+		subStr9.print();
+		assertEquals(6, subStr9.longestSubStr()[0]);
+		////////////////////////////////////////////
+		subStr10.createSubStr();
+
+		subStr10.print();
+		assertEquals(6, subStr10.longestSubStr()[0]);
+		////////////////////////////////////////////
+	}
+
+	@After
+	public void endMessage() {
+		String endMsg = "\n\nTests have ended in Success!!";
+		System.out.println(endMsg.toUpperCase());
+	}
+}
